4629945b7ab2fb9310d6b1a7615e70a0f424f012
ssongis <thddltmf97@naver.com>
2023-01-20 16:53:07 +0900

    usestate

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
-
-import Greeting from './components/Greeting';
-import FoodList from './components/FoodList';
+import Hello from "./components/Hello";
 
 function App() {
   return (
     <> 
-      <Greeting />
-      <FoodList />
+      <Hello />
     </>
   
   );
