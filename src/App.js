1e49af03c69e9cbbab6bc21ab418c26210e5b6cd
ssongis <thddltmf97@naver.com>
2023-01-25 10:24:50 +0900

    Item컴포넌트

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import Hello from "./components/Hello";
+import ItemMain from "./components/item/ItemMain";
 
 function App() {
   return (
     <> 
-      <Hello />
+      <ItemMain />
     </>
   
   );
