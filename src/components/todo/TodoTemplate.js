52a9e744899024367c535ba652278d00f9ea103c
ssongis <thddltmf97@naver.com>
2023-01-25 16:08:20 +0900

    input api

--- src/components/todo/TodoTemplate.js
+++ src/components/todo/TodoTemplate.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 // css 로딩
 import './css/TodoTemplate.css';
@@ -8,38 +8,42 @@ import TodoMain from './TodoMain';
 
 const TodoTemplate = () => {
 
-     // 할 일 api데이터
-  const todos = [
-    {
-        id: 1,
-        title: '아침 산책',
-        done: false
-    },
-    {
-        id: 2,
-        title: '오늘의 뉴스 읽기',
-        done: true
-    },
-    {
-        id: 3,
-        title: '샌드위치 사 먹기',
-        done: true
-    },
-    {
-        id: 4,
-        title: '리액트 공부하기',
-        done: true
-    },
-  ];
+  const API_BASE_URL = 'http://localhost:8080/api/todos';
+
+  // 할일 api데이터
+  const [todos, setTodos] = useState([]);
+
+  // 할 일 등록 서버 요청
+  const addTodo = (todo) => {
+    fetch(API_BASE_URL, {
+        method: 'POST',
+            headers: { 'content-type': 'application/json' },
+            body: JSON.stringify(todo) // todo를 input한테 받아와야 함
+    })
+    .then(res => res.json())
+    .then(result => {setTodos(result.todos)});
+  };
+
+  // 렌더링되자마자 할 일 => todos api GET 목록 호출
+  useEffect(() => {
+
+    fetch(API_BASE_URL)
+        .then(res => res.json())
+        .then(result => {
+            // console.log(result.todos);
+            setTodos(result.todos);
+        });
+
+  }, []);
 
 
-    return (
-      <div className="todo-template">
-          <TodoHeader todoList={todos}/>
-          <TodoMain todoList={todos}/>
-          <TodoInput />
-      </div>
-    )
-  }
-  
-  export default TodoTemplate
+  return (
+    <div className="todo-template">
+        <TodoHeader todoList={todos} />
+        <TodoMain todoList={todos} />
+        <TodoInput add={addTodo}/>
+    </div>
+  )
+}
+
+export default TodoTemplate
