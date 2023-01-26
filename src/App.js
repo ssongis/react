438760214c2e30c9fa42ca26c8b5be80d8c1c8c7
ssongis <thddltmf97@naver.com>
2023-01-26 17:05:28 +0900

    아이디,패스워드,이메일(회원가입)

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import ItemMain from "./components/item/ItemMain";
 import TodoTemplate from "./components/todo/TodoTemplate";
 import Header from "./components/layout/Header";
 import Footer from "./components/layout/Footer";
+import Join from './components/user/Join'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <> 
       <Header />
-      <TodoTemplate />
+      {/* <TodoTemplate /> */}
+      <Join />
       <Footer />
     </>
   
--- src/components/todo/TodoTemplate.js
+++ src/components/todo/TodoTemplate.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Spinner } from 'reactstrap';
 
 // css 로딩
 import './css/TodoTemplate.css';
@@ -6,12 +7,17 @@ import TodoHeader from './TodoHeader';
 import TodoInput from './TodoInput';
 import TodoMain from './TodoMain';
 
+import { BASE_URL, TODO } from '../../config/host-config';
+
 const TodoTemplate = () => {
 
-  const API_BASE_URL = 'http://localhost:8080/api/todos';
+  const API_BASE_URL = BASE_URL + TODO;
 
   // 할일 api데이터
   const [todos, setTodos] = useState([]);
+
+  // 로딩중 상태
+  const [loading, setLoading] = useState(true);
 
   // 할 일 등록 서버 요청
   const addTodo = (todo) => {
@@ -58,16 +64,38 @@ const TodoTemplate = () => {
   useEffect(() => {
 
     fetch(API_BASE_URL)
-        .then(res => res.json())
+        .then(res => {
+          if(res.status===403){
+            alert("로그인이 필요한 서비스입니다!");
+            // 리다이렉트
+            return;
+          }else if(res.status === 500){
+            alert("서버가 불안정합니다 ㅈㅅ");
+            return;
+          }
+          return res.json();
+        })
         .then(result => {
             // console.log(result.todos);
             setTodos(result.todos);
+
+            // 로딩완료처리
+            setLoading(false);
         });
 
   }, []);
 
+  // 로딩중일 때 보여줄 태그
+  const loadingPage = (
+    <div className="loading">
+      <Spinner color="danger">
+        Loading...
+      </Spinner>
+    </div>
+  );
 
-  return (
+  // 로딩완료시 보여줄 태그
+  const viewPage = (
     <div className="todo-template">
         <TodoHeader todoList={todos} />
         <TodoMain 
@@ -75,8 +103,15 @@ const TodoTemplate = () => {
             remove={deleteTodo} 
             update={updateTodo} 
         />
-        <TodoInput add={addTodo} />
+        <TodoInput add={addTodo} /> 
     </div>
+  );
+  
+
+  return (
+    <>
+      {loading ? loadingPage : viewPage}
+    </>
   );
 };
 
